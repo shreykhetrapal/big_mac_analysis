e8e4a7736630a9e8f9d4013d3e47eb1e274bb6cf
gibranerl <gibran.erlangga@gmail.com>
2021-11-11 15:30:25 -0500

    update code for preprocessing (big mac index & cattle) and data merging process

--- raw_preprocessing_big_mac.R
+++ raw_preprocessing_big_mac.R
@@ -1,29 +1,29 @@
 library('tidyverse')
 library('data.table')
 
-big_mac_countries = c('ARG', 'AUS', 'BRA', 'GBR', 'CAN', 'CHL', 'CHN', 'CZE', 'DNK',
-                      'EGY', 'HKG', 'HUN', 'IDN', 'ISR', 'JPN', 'MYS', 'MEX', 'NZL',
-                      'NOR', 'PER', 'PHL', 'POL', 'RUS', 'SAU', 'SGP', 'ZAF', 'KOR',
-                      'SWE', 'CHE', 'TWN', 'THA', 'TUR', 'ARE', 'USA', 'COL', 'CRI',
-                      'PAK', 'LKA', 'UKR', 'URY', 'IND', 'VNM', 'GTM', 'HND', 'VEN',
-                      'NIC', 'AZE', 'BHR', 'HRV', 'JOR', 'KWT', 'LBN', 'MDA', 'OMN',
-                      'QAT', 'ROU', 'EUZ')
+#big_mac_countries = c('ARG', 'AUS', 'BRA', 'GBR', 'CAN', 'CHL', 'CHN', 'CZE', 'DNK',
+#                      'EGY', 'HKG', 'HUN', 'IDN', 'ISR', 'JPN', 'MYS', 'MEX', 'NZL',
+#                      'NOR', 'PER', 'PHL', 'POL', 'RUS', 'SAU', 'SGP', 'ZAF', 'KOR',
+#                      'SWE', 'CHE', 'TWN', 'THA', 'TUR', 'ARE', 'USA', 'COL', 'CRI',
+#                      'PAK', 'LKA', 'UKR', 'URY', 'IND', 'VNM', 'GTM', 'HND', 'VEN',
+#                      'NIC', 'AZE', 'BHR', 'HRV', 'JOR', 'KWT', 'LBN', 'MDA', 'OMN',
+#                      'QAT', 'ROU', 'EUZ')
 
 big_mac_countries = c('ARG', 'CHN', 'DKK', 'CHF', 'IDN', 'USA')
-base_currencies = c('USD', 'EUR', 'GBP', 'JPY', 'CNY')
-base_currencies = c('USD')
+#base_currencies = c('USD', 'EUR', 'GBP', 'JPY', 'CNY')
+base_currencies = c('USD', 'CNY')
 
 # read data, sort by date and then by country name, for easy reading;
 # index on currency_code for faster joining, and
 # remove lines where the local price is missing
-big_mac_data = fread('./big-mac-source-data.csv', 
+big_mac_data = fread('./data_raw/big-mac-source-data.csv', 
                      na.strings = '#N/A',
                      key = 'date,name', 
                      index = 'currency_code') %>%
   .[!is.na(local_price)] %>%
   .[, !"GDP_dollar"]
 
-big_mac_data_old = fread('./big-mac-historical-source-data.csv', 
+big_mac_data_old = fread('./data_raw/big-mac-historical-source-data.csv', 
                      na.strings = '#N/A',
                      key = 'date,name', 
                      index = 'currency_code') %>%
@@ -38,9 +38,6 @@ big_mac_data <- rbind(big_mac_data, big_mac_data_old) %>% arrange(date, name)
 # convert to uniform currency (get dollar price for every country)
 big_mac_data$dollar_price = big_mac_data$local_price / big_mac_data$dollar_ex
 
-min(big_mac_data$date)
-head(big_mac_data)
-
 # remove lines where dollar price is missing
 # filter for listed countries in list above
 big_mac_index = big_mac_data[!is.na(dollar_price) & iso_a3 %in% big_mac_countries
@@ -54,9 +51,5 @@ for(currency in base_currencies) {
 # 
 big_mac_index[, (base_currencies) := lapply(.SD, round, 5L), .SDcols=base_currencies]
 
-head(big_mac_index[big_mac_index$iso_a3 %in% c('ARG', 'USA')])
-
-tail(big_mac_index)
-
-# calculation for adjusted index (based on GDP)
-
+# write adjusted data to /output_data folder
+fwrite(big_mac_index, './data_output/processed_bmi.csv')
--- raw_preprocessing_cattle.R
+++ raw_preprocessing_cattle.R
@@ -1,20 +1,58 @@
-library('tidyverse')
-library('data.table')
+library(janitor)
+library(lubridate)
+library(tidyverse)
+library(data.table)
+library(docstring)
 
-cattle_data = fread('./Live Cattle Futures Data - Sheet1.csv')
+# read csv, normalize column names, convert date column type into date, sort by date
+cattle_data = fread('./data_raw/Live Cattle Futures Data - Sheet1.csv') %>%
+  clean_names() %>%
+  mutate(date = date %>% mdy()) %>%
+  arrange(date)
 
-# convert date column type into date
-cattle_data$Date <- as.Date(cattle_data$Date, format="%m/%d/%Y")
-head(cattle_data)
+# add missing dates
+cattle_data_full_dates <- cattle_data %>% 
+                          complete(date = seq.Date(min(date), max(date), by="day"))
 
-# fill missing dates (min - max)
-cattle_data <- cattle_data %>% 
-               arrange(Date) %>%
-               complete(Date = seq.Date(min(Date), max(Date), by="day"))
 
-# fill missing values with the next day open price
-head(cattle_data,10)
+fill_missing_price <- function(data) {
+  #' for every date with missing adjusted close price value, replace NA with the nearest future date open price
+  #' 
+  #' replace the adj close price value with nearest open value of future date
+  temp = data
+  
+  # get dataframe of dates with missing values
+  na_dates <- temp %>% filter(is.na(open))
+  
+  # for every missing date, replace the adj close price value 
+  # with nearest open value of future date
+  for(i in 1:nrow(na_dates)){
+    
+    if (i %% 100 == 0) {
+      print(i)
+      }
+    
+    # get first non-NA open value in nearest future date
+    my_first_row <- temp %>% 
+                    filter(date >= na_dates$date[i]) %>%
+                    drop_na(open) %>% 
+                    slice(1) %>%
+                    pull(open)
+    
+    temp <- temp %>% 
+            mutate(adj_close = ifelse(date == na_dates$date[i],
+                                              my_first_row[1],
+                                              adj_close))
+  }
+  return(temp)
+}
 
-tail(cattle_data)
+adjusted_cattle_data <- fill_missing_price(cattle_data_full_dates)
 
-# merge between the big mac raw index and cattle data
+# filter to take only required columns
+final_data <- adjusted_cattle_data %>% select(date, adj_close)
+
+# write adjusted data to /output_data folder
+fwrite(final_data, './data_output/processed_cattle.csv')
+
+
